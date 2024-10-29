c425382b2b65bb25c658d18dba2a4508ac8023ae
yasmineelsheikh <yasmine.elsheikh8@gmail.com>
2024-10-29 14:41:24 -0700

    getting builder working

--- main.py
+++ main.py
@@ -19,9 +19,13 @@ from openvla.prismatic.vla.action_tokenizer import ActionTokenizer
 
 hf_base = "jxu124/OpenX-Embodiment"
 ucsd_kitchen_dataset_name = "ucsd_kitchen_dataset_converted_externally_to_rlds"
+# cmu_play_fusion_dataset_name = "cmu_play_fusion"
 # Load the dataset with the custom session
 # ds = datasets.load_dataset(
-#     hf_base, ucsd_kitchen_dataset_name, split="train", cache_dir="/workspaces/ares/data"
+#     hf_base,
+#     ucsd_kitchen_dataset_name,
+#     split="train",
+#     cache_dir="/workspaces/ares/data",
 # )
 # breakpoint()
 
@@ -30,27 +34,21 @@ ucsd_kitchen_dataset_name = "ucsd_kitchen_dataset_converted_externally_to_rlds"
 class EpisodicConfig:
     data_root_dir: Path = Path(
         # "datasets/open-x-embodiment"
-        f"data"
+        f"/workspaces/ares/data"
     )  # Path to Open-X dataset directory
     dataset_name: str = ucsd_kitchen_dataset_name
     image_sizes: tuple[int] = (224, 224)
     vla_path: str = "openvla/openvla-7b"  # Path to OpenVLA model (on HuggingFace Hub)
     shuffle_buffer_size: int = 256  # _000
     image_aug: bool = False
+    shuffle: bool = False
 
 
 cfg = EpisodicConfig()
 processor = AutoProcessor.from_pretrained(cfg.vla_path, trust_remote_code=True)
 action_tokenizer = ActionTokenizer(processor.tokenizer)
 
-batch_transform = RLDSBatchTransform(
-    action_tokenizer,
-    processor.tokenizer,
-    image_transform=processor.image_processor.apply_transform,
-    prompt_builder_fn=(
-        PurePromptBuilder if "v01" not in cfg.vla_path else VicunaV15ChatPromptBuilder
-    ),
-)
+batch_transform = lambda x: x
 vla_dataset = EpisodicRLDSDataset(
     cfg.data_root_dir,
     cfg.dataset_name,
@@ -59,4 +57,7 @@ vla_dataset = EpisodicRLDSDataset(
     shuffle_buffer_size=cfg.shuffle_buffer_size,
     image_aug=cfg.image_aug,
 )
-breakpoint()
+
+
+for ep in vla_dataset:
+    breakpoint()
