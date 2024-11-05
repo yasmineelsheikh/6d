6272d968bf1da76c51f2ef7f8a13020bdf23801e
yasmineelsheikh <yasmine.elsheikh8@gmail.com>
2024-11-05 15:51:36 -0800

    cleaning some

--- src/ares/configs/test_configs.py
+++ src/ares/configs/test_configs.py
@@ -1,18 +1,18 @@
-from configs import Environment, Robot, Task, Rollout, RolloutSQLModel
+from configs import Environment, Robot, Rollout, RolloutSQLModel, Task
 
 ROBOT1 = Robot(name="robot1", type="robot", sensor="camera")
 ENV1 = Environment(
     name="environment1", type="table", lighting="high light", simulation=True
 )
 TASK1 = Task(name="task1", description="task1", success_criteria="task1", success=0.9)
-TRAJ1 = Rollout(robot=ROBOT1, environment=ENV1, task=TASK1)
+ROLL1 = Rollout(robot=ROBOT1, environment=ENV1, task=TASK1)
 
 ROBOT2 = Robot(name="robot2", type="robot", sensor="camera")
 ENV2 = Environment(
     name="environment2", type="gym", lighting="low light", simulation=False
 )
 TASK2 = Task(name="task2", description="task2", success_criteria="task2", success=0.0)
-TRAJ2 = Rollout(robot=ROBOT2, environment=ENV2, task=TASK2)
+ROLL2 = Rollout(robot=ROBOT2, environment=ENV2, task=TASK2)
 
-SQL_TRAJ1 = RolloutSQLModel(**TRAJ1.flatten_fields(""))
-SQL_TRAJ2 = RolloutSQLModel(**TRAJ2.flatten_fields(""))
+SQL_ROLL1 = RolloutSQLModel(**ROLL1.flatten_fields(""))
+SQL_ROLL2 = RolloutSQLModel(**ROLL2.flatten_fields(""))
