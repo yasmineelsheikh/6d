fec0c1c6369d01bf9822df53fe2f0aeee9c36b7e
yasmineelsheikh <yasmine.elsheikh8@gmail.com>
2024-11-04 17:04:53 -0800

    trying to fix formatting :(

--- src/ares/task_utils.py
+++ src/ares/task_utils.py
@@ -2,46 +2,46 @@ PI_DEMO_PATH = "/workspaces/ares/data/pi_demos"
 PI_DEMO_TASKS = {
     "Eggs in carton": {
         "task": "The robot must place every egg into the carton and then close and secure the lid.",
-        "filename_prefix": "processed_eggs"
+        "filename_prefix": "processed_eggs",
     },
     "Grocery Bagging": {
         "task": "The robot must place every item into the bag.",
-        "filename_prefix": "processed_grocery_bagging"
+        "filename_prefix": "processed_grocery_bagging",
     },
     "Toast out of toaster": {
         "task": "The robot must remove the toast from the toaster and place it on the plate.",
-        "filename_prefix": "processed_toast"
+        "filename_prefix": "processed_toast",
     },
     "Towel fold": {
         "task": "The robot must fold the towel.",
-        "filename_prefix": "processed_towel_fold"
+        "filename_prefix": "processed_towel_fold",
     },
     "Stack bowls": {
         "task": "The robot must stack all of the bowls.",
-        "filename_prefix": "processed_stack"
+        "filename_prefix": "processed_stack",
     },
     "Tupperware in microwave": {
         "task": "The robot must place the tupperware in the microwave and close the door.",
-        "filename_prefix": "processed_tupperware"
+        "filename_prefix": "processed_tupperware",
     },
     "Items in drawer": {
         "task": "The robot must place all of the items in the drawer and close the drawer.",
-        "filename_prefix": "processed_drawer"
+        "filename_prefix": "processed_drawer",
     },
     "Laundry fold (shirts)": {
         "task": "The robot must neatly fold the shirt.",
-        "filename_prefix": "processed_folding_single_shirt"
+        "filename_prefix": "processed_folding_single_shirt",
     },
     "Laundry fold (shorts)": {
         "task": "The robot must neatly fold the shorts.",
-        "filename_prefix": "processed_fold_single_shorts"
+        "filename_prefix": "processed_fold_single_shorts",
     },
     "Paper towel in holder": {
         "task": "The robot must place the paper towel roll completely onto the holder.",
-        "filename_prefix": "processed_towel"
+        "filename_prefix": "processed_towel",
     },
     "Food in to go box": {
         "task": "The robot must place the food in the to go box and properly close the lid.",
-        "filename_prefix": "processed_togo"
+        "filename_prefix": "processed_togo",
     },
 }
